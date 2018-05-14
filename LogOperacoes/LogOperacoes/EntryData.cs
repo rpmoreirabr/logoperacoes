0a8ca57375e4bc7294d1d0d2b892f498383d2065
Rodrigo Paixao Moreira da Costa <romoreira@mongeralaegon.com.br>
2018-05-14 11:09:31 -0300

    Initial commit

---
+++ LogOperacoes/LogOperacoes/EntryData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogOperacoes
+{
+    public class EntryData
+    {
+        public string applicationName { get; set; }
+        public string serviceName { get; set; }
+        public string url { get; set; }
+        public HttpMethod method { get; set; }
+        public string correlationKey { get; set; }
+        public DateTime entryTime { get; set; }
+        public long? duration { get; set; }
+        public string requestContent { get; set; }
+        public string responseContent { get; set; }
+        public string client { get; set; }
+        public HttpStatusCode? responseStatusCode { get; set; }
+        public IEnumerable<string> metadados { get; set; }
+        public bool success { get; set; }
+        public string user { get; set; }
+    }
+}
---
+++ LogOperacoes/LogOperacoes/EntryRecorder.cs
@@ -0,0 +1,94 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LogOperacoes
+{
+    public class EntryRecorder
+    {
+        private ILogger Logger;
+
+        public EntryRecorder()
+        {
+            Logger = new LoggerConfiguration()
+              .MinimumLevel.Debug()
+              .ReadFrom
+              .AppSettings()
+              .CreateLogger();
+        }
+        public void LogDataAsync(string url,
+            HttpMethod method,
+            string correlationKey,
+            DateTime entryTime,
+            string requestContent,
+            string responseContent,
+            string client,
+            HttpStatusCode responseStatusCode,
+            bool success,
+            IEnumerable<string> metadados = null,
+            string user = null,
+            long? duration = null,
+            string applicationName = null,
+            string serviceName = null)
+        {
+            var regexUri = new Regex(@"http[s]?://\w*:?\w*/(?<application>[\w\.]*)/(?<service>[^?]*)\?*(?<query>.*)", RegexOptions.Compiled);
+
+            var entry = new EntryData()
+            {
+                url = url,
+                client = client,
+                requestContent = requestContent,
+                responseContent = responseContent,
+                correlationKey = correlationKey,
+                entryTime = entryTime,
+                metadados = metadados,
+                method = method,
+                responseStatusCode = responseStatusCode,
+                success = success,
+                user = user,
+                duration = duration,
+                applicationName = applicationName ?? regexUri.Match(url)?.Groups["application"]?.Value,
+                serviceName =  serviceName ?? regexUri.Match(url)?.Groups["service"]?.Value
+            };
+
+            if (success)
+            {
+                Logger.Information("{url} {applicationName} {serviceName} {requestContent} {responseContent} {duration} {correlationKey} {entryTime} {method} {statusCode} {client} {metadata}",
+                    entry.url,
+                    entry.applicationName,
+                    entry.serviceName,
+                    entry.requestContent,
+                    entry.responseContent,
+                    entry.duration,
+                    entry.correlationKey,
+                    entry.entryTime,
+                    entry.method,
+                    entry.responseStatusCode,
+                    entry.client,
+                    entry.metadados);
+            }
+            else
+            {
+                Logger.Error("{url} {applicationName} {serviceName} {requestContent} {responseContent} {duration} {correlationKey} {entryTime} {method} {statusCode} {client} {metadata}",
+                    entry.url,
+                    entry.applicationName,
+                    entry.serviceName,
+                    entry.requestContent,
+                    entry.responseContent,
+                    entry.duration,
+                    entry.correlationKey,
+                    entry.entryTime,
+                    entry.method,
+                    entry.responseStatusCode,
+                    entry.client,
+                    entry.metadados);
+            }
+        }
+    }
+}
